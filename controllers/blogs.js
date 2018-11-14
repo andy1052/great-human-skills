774528e030cef0f5677cb12226773319ffec77f2
andy1052 <andy.ducharme@gmail.com>
2018-11-14 06:49:08 -0700

    Admin can now save blog form to the db

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -53,17 +53,15 @@ app.post('/adlog', async (req, res) => {
 });
 
 
-//	New Blog Post Route:
+//	New Blog LOGIN Post Route:
 app.post('/newBlog', async (req, res) => {
 	//	Sanitize the data
 	let username = typeof(req.body.username) === "string" && req.body.username.trim().length > 0 && req.body.username.trim().length < 80 ? req.body.username.trim() : false;
 	let password = typeof(req.body.password) === "string" && req.body.password.trim().length > 0 && req.body.password.trim().length < 60 ? req.body.password.trim() : false;
 
 	try {
-		//	Find this email in the database:
+		//	Find this username and password in the database:
 		let check = await dbFuncs.find({"username": username, "password": password}, 'blogger').then((result) => {
-
-console.log("result from blogs: ", result);
 
 			if (!result) {
 				//	User not found:
@@ -71,35 +69,21 @@ console.log("result from blogs: ", result);
 			}
 			return result;
 		});
+
 			//	If there is no check, throw error:
 			if (!check) throw new Error({"Error": "Cannot login, non-existent user."});
-
-			//	Compare entered password to hashed password:
-		//	let c = await helpers.compare(password, password).then((result) => {
-		//		if (!result) {
-					//	Password not found:
-
-					//*********** TODO ************* display a message on login page to try again.
-
-		//			return res.status(401).res.redirect('/login');
-		//		}
 
 				//	Otherwise, create a new token:
 				let token = jwt.sign({_id: check._id}, process.env.SECRET, {expiresIn: "60 days"});
 				//	Then set a cookie and redirect to homepage:
 				res.cookie('nToken', token, {maxAge: 900000, httpOnly: true});
-				// console.log("Password result: ", result);
-				// console.log("token:", token);
-		//		return result;
-		//	});
 
-			//	Overkill check on c, if no result is returned, throw error:
-		//	if (!c) throw new Error({"Error": "No result returned from checking password, internal error"});
-
-//let admin = c; 
-console.log("From blogs.js: ", token);
-			//	At this point, everything went well, and you should send the user to the newBlog page along with req.user
-			res.render('blog', {token});
+			//	At this point, everything went well, and you should send the user to the Blog page along with the token
+			if (token) {
+				return res.render('blog', {token});
+			} else {
+				return res.status(401).res.redirect('/unauthorized');
+			};
 
 			} catch(e) {
 				console.log(e.stack);
@@ -108,6 +92,68 @@ console.log("From blogs.js: ", token);
 
 		});
 
+
+
+//	Save New Blog To The Database Route:
+app.post('/blogSave', async (req, res) => {
+
+	//	First sanitize the data:
+	let title = typeof(req.body.title) === "string" && req.body.title.trim().length >0 && req.body.title.trim().length < 60 ? req.body.title.trim() : false;
+	let author = typeof(req.body.author) === "string" && req.body.author.trim().length >0 && req.body.author.trim().length < 60 ? req.body.author.trim() : false;
+	let description = typeof(req.body.description) === "string" && req.body.description.length >0 && req.body.description.length < 500 ? req.body.description : false;
+	let blog = typeof(req.body.blog) === "string" && req.body.description.length > 0 ? req.body.blog : false;
+
+console.log(req.body);
+console.log("req.user from blogs.js", req.user);
+	try {
+	if (req.user) {
+
+	//	Once data is sanitized, make object and save it to database:
+	if (title && author && description && blog) {
+
+	//	Make object:
+	const article = {
+		title,
+		author,
+		description,
+		blog
+	};
+
+
+
+		let x = await dbFuncs.insert(article, "articles").then((result) => {
+
+			//	If database confirms save.
+			if({"n":1, "ok":1}) {
+			console.log("Saved to database!");
+			return result;
+		} else {
+			//	Else, send error
+			res.status(400).res.send("Something went wrong when trying to save to database");
+		}
+	});
+
+		//	Overkill check:
+		if (!x) throw new Error({"Error": "Something failed in blogSave"});
+
+		// ************ TODO ************************
+		// Should you be automatically logged out right here????
+
+
+		res.redirect('/');
+
+	} else {
+		return res.status(401).res.send("Oops! Something went wrong. Cannot save to database");
+	};
+}else {
+	return res.send("Sorry, there is no user present.");
+};
+} catch(e){
+	console.log(e);
+	return e;
+}
+
+});
 
 
 };	//	End of module exports.
