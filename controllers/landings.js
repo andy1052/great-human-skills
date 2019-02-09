00d3e5012dee3ce38d45c591a9a071501a85cff2
andy1052 <andy.ducharme@gmail.com>
2019-02-08 23:58:09 -0700

    completely redesigned a responsive ad for book and updated links and routes accordingly

--- controllers/landings.js
+++ controllers/landings.js
@@ -15,28 +15,11 @@
 exports = module.exports = function(app) {
 
 
-//	Get Words are all you've got landing page:
-app.get('/allYouveGot', (req, res, next) => {
 
+//	Route for wordsAd:
+app.get('/wordsAd', (req, res, next) => {
 	try {
-		res.render('allYouveGot');
-	} catch(e) {
-		console.error(e);
-		next(e);
-	};
-});
-
-
-
-//	Route for books:
-app.get('/books', (req, res, next) => {
-
-	try {
-
-		let currentUser = req.user;
-
-		res.render('books', {currentUser});
-
+		res.render('wordsAd');
 	} catch(e) {
 		console.error(e);
 		next(e);
